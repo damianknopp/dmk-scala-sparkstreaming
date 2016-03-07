13667d4c71cb889064dc65f9e1909977194adeb9
Damian Knopp <tribeknopp@gmail.com>
2016-03-06 20:11:41 -0500

    readme++

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dmk-scala-sparkstreaming
+
+To build
+---
+cd $SPARK_HOME
+dmk-scala-sparkstreaming/helper-scripts/build-and-deploy.sh
+
+To run
+---
+./dmk-run-fstreamtest.sh baseball-in/ baseball-out/
+
+cp basebase.csv baseball-in/.
--- helper-scripts/build-deploy-local.sh
+++ helper-scripts/build-deploy-local.sh
@@ -5,5 +5,8 @@ CODE=$HOME/workspace-e4.4/dmk-scala-sparkstreaming
 pushd $CODE
 mvn clean package
 cp target/*.jar $SPARK_HOME
+mkdir -p tmp-data-split
+cp data/*.csv tmp-data-split
+pushd tmp-data-split && split -l baseball.csv && popd
 cp helper-scripts/*.sh $SPARK_HOME
 popd
